8ccfdddaf58b98e8769f7878e5110851af217b54
Justin BIZUMUREMYI <bizujustin1234@gmail.com>
2023-01-12 03:30:52 +0200

    Update settings.py

--- NPD_TCSS/settings.py
+++ NPD_TCSS/settings.py
@@ -70,6 +70,7 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
